abd96d097f1158a2f3b9fc5c1db45fa9eff2ff98
limit <limit@limits-mbp.home>
2019-06-11 10:53:37 +0100

    created game logic

--- memory_game/js/main.js
+++ memory_game/js/main.js
@@ -1,7 +1,18 @@
 console.log("Up and running!");
-var cardOne = "queen";
-var cardTwo = "queen";
-var cardThree = "king";
-var cardFour = "king";
 
+var cards = ["queen", "queen", "king", "king"];
+var cardsInPlay = [];
+var cardOne = cards[0];
+cardsInPlay.push(cardOne);
 console.log("User flipped " + cardOne);
+
+var cardTwo = cards [2];
+cardsInPlay.push(cardTwo);
+console.log("User flipped " + cardTwo);
+
+
+if (cardsInPlay.length === 2 && cardsInPlay[0] === cardsInPlay[1]) {
+	alert("You found a match"); 
+} else {
+	alert("Sorry, try again");
+}
