d677dcba57f12f8a51ab260812195b0c44b1ee4c
limit <limit@limits-mbp.home>
2019-06-12 20:02:37 +0100

    created game logic

--- memory_game/js/main.js
+++ memory_game/js/main.js
@@ -24,6 +24,18 @@ var cards = [
 ];
 
 var cardsInPlay = [];
+
+function createBoard() {
+	for (var i = 0; i < cards.length; i++) {
+		var cardElement = document.createElement ('img');
+			cardElement.setAttribute('src', 'images/back.png');
+			cardElement.setAttribute('data-id', i);
+			cardElement.addEventListener('click', flipCard);
+			document.getElementById('game-board').appendChild(cardElement)
+	}
+
+}
+
 function checkForMatch() {
 if (cardsInPlay[0] === cardsInPlay[1]) {
 	alert("You found a match"); 
@@ -32,19 +44,20 @@ if (cardsInPlay[0] === cardsInPlay[1]) {
 }
 }
 
-function flipCard(cardId) {
+function flipCard() {
+	var cardId = this.getAttribute('data-id');
+	console.log("User flipped " + cards[cardId].rank);
+	console.log(cards[cardId].cardImage);
+	cardsInPlay.push(cards[cardId].rank);
+	this.setAttribute('src', cards[cardId].cardImage);
 if (cardsInPlay.length ===2) {
 	checkForMatch ();
+
 }
-	console.log("User flipped " + cards[cardId].rank);
-	cardsInPlay.push(cards[cardId].rank);
-	console.log(cards[cardId].cardImage);
-	console.log(cards[cardId].suit);
 }
 
-flipCard(0);
-flipCard(2);
-checkForMatch();
+createBoard();
+
 
 
 
