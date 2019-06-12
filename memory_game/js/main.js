e8bcbe50a4cc09db7837f57972f4f37b27ba0c0e
limit <limit@limits-mbp.home>
2019-06-12 14:58:08 +0100

    created game logic

--- memory_game/js/main.js
+++ memory_game/js/main.js
@@ -1,13 +1,34 @@
 console.log("Up and running!");
 
-var cards = ["queen", "queen", "king", "king"];
+var cards = [
+{
+	rank: "queen",
+	suit: "hearts", 
+	cardImage: "images/queen-of-hearts.png",
+},
+{
+	rank: "queen",
+	suit: "diamonds", 
+	cardImage: "images/queen-of-diamonds.png",
+},
+{
+	rank: "king",
+	suit: "hearts", 
+	cardImage: "images/king-of-hearts.png",
+},
+{
+	rank: "king",
+	suit: "diamonds", 
+	cardImage: "images/king-of-diamonds.png",
+}
+];
+
 var cardsInPlay = [];
 function checkForMatch() {
-//if (cardsInPlay.length === 2 &&
 if (cardsInPlay[0] === cardsInPlay[1]) {
-	console.log("You found a match"); 
+	alert("You found a match"); 
 } else {
-	console.log("Sorry, try again");
+	alert("Sorry, try again");
 }
 }
 
@@ -15,13 +36,16 @@ function flipCard(cardId) {
 if (cardsInPlay.length ===2) {
 	checkForMatch ();
 }
-	console.log("User flipped " + cards[cardId]);
-	cardsInPlay.push(cards[cardId]);
+	console.log("User flipped " + cards[cardId].rank);
+	cardsInPlay.push(cards[cardId].rank);
+	console.log(cards[cardId].cardImage);
+	console.log(cards[cardId].suit);
 }
 
 flipCard(0);
 flipCard(2);
 checkForMatch();
+
 
 
 
